9d0ed8a2d560a2eb731588870bc5d2be59aa06a0
Khaair <khair.cse.iu@gmail.com>
2021-11-16 17:22:15 +0600

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 
-var student1={
-    name: "Khair",
-    age: 26,
-    lang: ["Bangla","English"]
-
+function Student(name,age,cgpa,lang){
+    this.name=name;
+    this.age=age;
+    this.cgpa=cgpa;
+    this.lang=lang;
 }
+
+var student1=new Student("Khair",23,3.25,["Bangla","English"]);
+var student2=new Student("Khair",23,3.25,["Bangla","English"]);
+var student3=new Student("Khair",23,3.25,["Bangla","English"]);
 console.log(student1.name);
+console.log(student2.age);
+console.log(student3.lang);
 
-var student2={
-    name: "Rakib",
-    age: 26,
-    lang: ["Bangla","English"]
 
-}
-console.log(student2.name);
+// var student1={
+//     name: "Khair",
+//     age: 26,
+//     lang: ["Bangla","English"]
 
-var student3={
-    name: "Khair",
-    age: 26,
-    lang: ["Bangla","English"]
+// }
+// console.log(student1.name);
 
-}
-console.log(student3.lang[1]);
+// var student2={
+//     name: "Rakib",
+//     age: 26,
+//     lang: ["Bangla","English"]
+
+// }
+// console.log(student2.name);
+
+// var student3={
+//     name: "Khair",
+//     age: 26,
+//     lang: ["Bangla","English"]
+
+// }
+// console.log(student3.lang[1]);
 
