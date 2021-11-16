27355354e559dbf4d91bc7b9da4256d91b877d3b
Khaair <khair.cse.iu@gmail.com>
2021-11-16 15:43:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var student1={
+    name: "Khair",
+    age: 26,
+    lang: ["Bangla","English"]
+
+}
+console.log(student1.name);
+
+var student2={
+    name: "Rakib",
+    age: 26,
+    lang: ["Bangla","English"]
+
+}
+console.log(student2.name);
+
+var student3={
+    name: "Khair",
+    age: 26,
+    lang: ["Bangla","English"]
+
+}
+console.log(student3.lang[1]);
+
